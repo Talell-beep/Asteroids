21a3b06af946298768d8a466fc638902297361c5
Talell <d.crombied@pm.me>
2024-11-01 19:39:03 +0000

    Initial push to Github

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
+    framelimit = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = framelimit.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
